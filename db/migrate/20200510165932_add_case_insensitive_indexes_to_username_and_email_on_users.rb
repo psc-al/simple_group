4127135b7d82b9d0338139939a38b42029c88aa0
Anthony F. Hernandez <ant@antfeedr.com>
2020-05-10 10:08:01 -0700

    user functional indexes on username and email
    This commit updates the indexes on `username` and
      `email` on the `users` table to be functional indexes
      that user the `LOWER` function to allow for case-insensitive
      exact matches.
    
    We could have used `citext` for this, but that is postgres-specific,
      and there is desire amongst the community to be able to port the
      application to other DBMSs in the future without huge issues (I
      realize that it will still be a pain in the ass, but I'll do what I
      can to support this regardless).

---
+++ db/migrate/20200510165932_add_case_insensitive_indexes_to_username_and_email_on_users.rb
@@ -0,0 +1,17 @@
+class AddCaseInsensitiveIndexesToUsernameAndEmailOnUsers < ActiveRecord::Migration[6.0]
+  def up
+    remove_index :users, :email
+    remove_index :users, :username
+
+    add_index :users, "LOWER(email)", unique: true, name: "index_users_on_LOWER_email"
+    add_index :users, "LOWER(username)", unique: true, name: "index_users_on_LOWER_username"
+  end
+
+  def down
+    remove_index :users, name: "index_users_on_LOWER_email"
+    remove_index :users, name: "index_users_on_LOWER_username"
+
+    add_index :users, "email", unique: true
+    add_index :users, "username", unique: true
+  end
+end

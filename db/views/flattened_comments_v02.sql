87aeb368acfc2008d864df10ed8e8174d0e9f536
Anthony F. Hernandez <ant@antfeedr.com>
2020-05-22 14:49:45 -0700

    adds votable score information to flattened database views
    this commit adds the score to flattened submissions / comments,
      which is the difference between the number of upvotes and downvotes.

---
+++ db/views/flattened_comments_v02.sql
@@ -0,0 +1,20 @@
+SELECT
+  comments.id, comments.short_id, submission_id, parent_id,
+  body, comments.created_at, comments.updated_at,
+  users.username AS commenter,
+  (
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Comment'
+      AND votable_id = comments.id AND kind = 0
+    ) - 
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Comment'
+      AND votable_id = comments.id AND kind = 1
+    ) 
+  ) AS score
+FROM comments
+INNER JOIN users ON users.id = comments.user_id
---
+++ db/views/flattened_submissions_v02.sql
@@ -0,0 +1,56 @@
+SELECT
+  submissions.id, submissions.short_id, title, url, domains.name AS domain_name,
+  NULL AS body, users.username AS submitter_username, original_author, submissions.created_at,
+  (
+      SELECT COUNT(submission_id) AS comment_count
+      FROM comments
+      WHERE comments.submission_id = submissions.id
+    ) AS comment_count,
+  (
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Submission'
+      AND votable_id = submissions.id AND kind = 0
+    ) - 
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Submission'
+      AND votable_id = submissions.id AND kind = 1
+    ) 
+  ) AS score
+FROM
+  submissions
+INNER JOIN users ON submissions.user_id = users.id
+INNER JOIN domains ON submissions.domain_id = domains.id
+WHERE submissions.domain_id IS NOT NULL
+
+UNION ALL
+
+SELECT
+  submissions.id, submissions.short_id, title, NULL AS url, NULL AS domain_name,
+  submissions.body, users.username AS submitter_username, original_author, submissions.created_at,
+  (
+      SELECT COUNT(submission_id) AS comment_count
+      FROM comments
+      WHERE comments.submission_id = submissions.id
+    ) AS comment_count,
+  (
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Submission'
+      AND votable_id = submissions.id AND kind = 0
+    ) - 
+    (
+      SELECT COUNT(votable_id)
+      FROM votes
+      WHERE votable_type = 'Submission'
+      AND votable_id = submissions.id AND kind = 1
+    ) 
+  ) AS score
+FROM
+  submissions
+INNER JOIN users ON submissions.user_id = users.id
+WHERE submissions.domain_ID IS NULL
